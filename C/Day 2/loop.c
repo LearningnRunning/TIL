1e7e9047b594ab24d5bff54056b0d4567f96325f
Rocki_Kim <rocki.k1208@gmail.com>
2023-06-06 11:33:50 +0900

    TIL: C

--- C/Day 2/loop.c
+++ C/Day 2/loop.c
@@ -62,16 +62,6 @@ int main(void) {
         printf("\n");
     }
 
-    #include <stdio.h>
-    #define LEN_INPUT 11
-
-    int main(void) {
-        char s1[LEN_INPUT];
-        int a;
-        scanf("%s %d", s1, &a);
-        for (int i = 0; i < a; ++i) {
-            printf("%s", s1);
-        }
 
     return 0;
 }
